c8c167a89e889da1a8ff073ede06f246eb98f052
Angel <jose.prieto@levva.io>
2023-02-26 10:03:46 -0300

    feat: implement card color

--- src/components/Card/styles.tsx
+++ src/components/Card/styles.tsx
@@ -11,6 +11,75 @@ const CardLayout = styled.div`
   @media (max-width: 475px) {
     width: 260px;
   }
+
+  @property --rotate {
+    syntax: "<angle>";
+    initial-value: 132deg;
+    inherits: false;
+  }
+  
+  & {
+  background: #191c29;
+  position: relative;
+  border-radius: 6px;
+  cursor: pointer;
+  margin-bottom: 5px;
+  width: 100%;
+}
+
+.card:hover {
+  color: rgb(88 199 250 / 100%);
+  transition: color 1s;
+}
+&:hover:before, &:hover:after {
+  animation: none;
+  opacity: 0;
+}
+
+
+&:before {
+  content: "";
+  width: 105%;
+  height: 102%;
+  border-radius: 8px;
+  background-image: linear-gradient(
+    var(--rotate)
+    , #5ddcff, #3c67e3 43%, #4e00c2);
+    position: absolute;
+    z-index: -1;
+    top: -1%;
+    left: -2%;
+    animation: spin 2.5s linear infinite;
+}
+
+&:after {
+  position: absolute;
+  content: "";
+  top: calc(var(--card-height) / 6);
+  left: 0;
+  right: 0;
+  z-index: -1;
+  height: 100%;
+  width: 100%;
+  margin: 0 auto;
+  transform: scale(0.8);
+  filter: blur(calc(var(--card-height) / 6));
+  background-image: linear-gradient(
+    var(--rotate)
+    , #5ddcff, #3c67e3 43%, #4e00c2);
+    opacity: 1;
+  transition: opacity .5s;
+  animation: spin 2.5s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    --rotate: 0deg;
+  }
+  100% {
+    --rotate: 360deg;
+  }
+}
 `;
 
 const CardButton = styled.span`
